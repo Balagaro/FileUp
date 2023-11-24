4bd4476b8ca660a60ad4c34df27eaf1271925ab5
Balagaro <kiraly123balazs@gmail.com>
2023-11-24 22:10:54 +0100

    UploadPage V0.116

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app=express()
 app.use(express.static(__dirname+'/FRONTEND_DEMO'));
 
 app.get('/', function (req, res){
-    res.sendFile(path.join('index.html'))
+    res.render('index', { title: 'Hey', message: 'Hello there!' })
 });
-app.listen(8080)
-console.log('megyunk elore hajra Szilard')
+app.listen(3000)
+console.log('hajra Szilard')
