3a506bbb84014cf17c87e913aa17103fb1e0e57a
Korom Szilárd <korom.szilard@gmail.com>
2023-11-25 14:41:46 +0100

    FileShare demo application

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const http = require('http');
 const fs = require('fs');
-const express=require('express')
+const express = require('express')
 const path = require('path')
 
-const app=express()
-app.use(express.static(__dirname+'/FRONTEND_DEMO'));
+const app = express()
+app.use(express.static(__dirname + '/FRONTEND_DEMO'));
 
-app.get('/', function (req, res){
+app.get('/', function (req, res) {
     res.render('index', { title: 'Hey', message: 'Hello there!' })
 });
 app.listen(3000)
