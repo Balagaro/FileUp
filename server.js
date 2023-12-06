4b8c25c7a631018aec2f62989bc71963393c12ff
Balagaro <kiraly123balazs@gmail.com>
2023-12-06 22:56:18 +0100

    Fogadás gomb újragondolása

--- server.js
+++ server.js
@@ -12,19 +12,24 @@ app.use(express.static(path.join(__dirname+"/public")));
 io.on("connection", function(socket){
     socket.on("sender-join", function(data){
         socket.join(data.uid);
+        console.log("meta1")
     });
     socket.on("receiver-join", function(data){
         socket.join(data.uid);
         socket.in(data.sender_uid).emit("init",data.uid);
+        console.log("meta2")
     });
     socket.on("file-meta", function(data){
         socket.in(data.uid).emit("fs-meta",data.metadata);
+        console.log("meta3")
     });
     socket.on("fs-start", function(data){
         socket.in(data.uid).emit("fs-share",{});
+        console.log("meta4")
     });
     socket.on("file-raw", function(data){
         socket.in(data.uid).emit("fs-share",data.buffer);
+        console.log("meta5")
     });
 
 
