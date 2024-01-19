50775e535f88497e478ba3f9518758f5ea3c9ffa
Balagaro <kiraly123balazs@gmail.com>
2024-01-19 19:09:32 +0100

    Get the ip of the user

--- server.js
+++ server.js
@@ -9,14 +9,20 @@ const io = require("socket.io")(server);
 const app1 = express.Router()
 const app2 = express.Router()
 
-app1.use(express.static(path.join(__dirname+"/public")));
-app2.use(express.static(path.join(__dirname+"/public/mobile")));
+app1.use((req, res, next) => {
+    const ipAddress = req.socket.remoteAddress;
+    next();
+});
+app1.use('/', express.static(path.join(__dirname+"/public")))
 
-app.use(vhost('fileup.site', app1))
+app2.use((req, res, next) => {
+    const ipAddress = req.socket.remoteAddress;
+    next();
+});
+app2.use('/', express.static(path.join(__dirname+"/public/mobile")))
+
+app.use(vhost('104.248.92.11', app1))
 app.use(vhost('m.fileup.site', app2))
-
-/*app.use(express.static(path.join(__dirname+"/public")));*/
-
 
 io.on("connection", function(socket){
     socket.on("sender-join", function(data){
@@ -25,6 +31,9 @@ io.on("connection", function(socket){
     socket.on("receiver-join", function(data){
         socket.join(data.uid);
         socket.in(data.sender_uid).emit("init",data.uid);
+    });
+    socket.on("niggas", function (data){
+        console.log(data)
     });
     socket.on("reveive-joined", function(data){
         socket.in(data.uid).emit("rev-joined",data.uid)
@@ -39,9 +48,6 @@ io.on("connection", function(socket){
         socket.in(data.uid).emit("fs-share",[data.buffer, data.metadata]);
     });
 })
-
-
-
 
 server.listen(80);
 console.log("SziaSzilard")
--- testserver.js
+++ testserver.js
@@ -3,20 +3,66 @@ const path = require("path");
 const JSZip=require('jszip');
 const FileSaver =require('file-saver');
 const app = express();
-//import vhost from 'vhost'
-const vhost=require('vhost')
 const server = require("http").createServer(app);
-
+const vhost=require('vhost')
+const io = require("socket.io")(server);
 const app1 = express.Router()
 const app2 = express.Router()
 
+
+app1.use((req, res, next) => {
+    const ipAddress = req.socket.remoteAddress;
+    next();
+});
+app1.use('/', express.static(path.join(__dirname+"/public")))
+
+
+app2.use((req, res, next) => {
+    const ipAddress = req.socket.remoteAddress;
+    next();
+});
+app2.use('/', express.static(path.join(__dirname+"/public/mobile")))
+/*
+var asdasd = function(req, res, next) {
+	express.static(path.join(__dirname+"/public"));
+	  // ... perform some operations
+	  next(); // Call next() so Express will call the next middleware function in the chain.
+}
+
 app1.use(express.static(path.join(__dirname+"/public")));
-app2.use(express.static(path.join(__dirname+"/public/mobile")));
+app2.use(express.static(path.join(__dirname+"/public/mobile")));*/
 
-app.use(vhost('localhost', app1))
-app.use(vhost('m.localhost', app2))
+/*
+app1.get('/', (req, res) => {
+  const ipAddress = req.socket.remoteAddress;
+   console.log(ipAddress);
+	console.log("alma");
+})*/
 
-const io = require("socket.io")(server);
+/*
+// requests will never reach this route
+app1.get('/', (req, res) => {
+	const ipAddress = req.socket.remoteAddress;
+    console.log(ipAddress);
+	res.sendFile(path.join(__dirname, 'index.html'));
+
+})
+*/
+/*
+
+app1.use(express.static(path.join(__dirname+"/public/mobile")),function(req, res, next) {
+    const ipAddress = req.socket.remoteAddress;
+    console.log(ipAddress);
+	next();
+});*/
+
+/*app.use(express.static(path.join(__dirname+"/public")), app1)*/
+
+app.use(vhost('104.248.92.11', app1))
+app.use(vhost('m.fileup.site', app2))
+
+/*app.use(express.static(path.join(__dirname+"/public")));*/
+
 
 io.on("connection", function(socket){
     socket.on("sender-join", function(data){
@@ -25,6 +71,9 @@ io.on("connection", function(socket){
     socket.on("receiver-join", function(data){
         socket.join(data.uid);
         socket.in(data.sender_uid).emit("init",data.uid);
+    });
+    socket.on("niggas", function (data){
+        console.log(data)
     });
     socket.on("reveive-joined", function(data){
         socket.in(data.uid).emit("rev-joined",data.uid)
@@ -39,7 +88,6 @@ io.on("connection", function(socket){
         socket.in(data.uid).emit("fs-share",[data.buffer, data.metadata]);
     });
 })
-
 
 
 
