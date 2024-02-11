4d5b8f512e54b399cf366e96623e016594aa7f42
Balagaro <kiraly123balazs@gmail.com>
2024-02-11 19:57:47 +0100

    Sender can upload files before connection

--- public/send.js
+++ public/send.js
@@ -1,4 +1,7 @@
 let stoppedlist=[];
+
+let connected=[false, false];
+let already=[];
 function megallit(e){
     e.innerHTML="<img src='/docs/assets/start.svg'>";
     e.setAttribute('onclick','elindit(this)')
@@ -43,9 +46,28 @@ socket.on("init", function(uid){
     receiverID = uid;
     document.querySelector(".waitingfor").classList.remove("active");
     document.querySelector(".readyfor").classList.add("active");
+    connected[0]=true;
+    if (connected[1]===true){
+        document.querySelector(".readyfor").classList.remove("active");
+        document.querySelector(".fileok").classList.add("active");
+        for (let i = 0; i < already.length; i++){
+            let currentelement=already[i];
+            let alrel=currentelement[0]
+            shareFile({
+                    filename:currentelement[1],
+                    total_buffer_size:currentelement[2].length,
+                    buffer_size:1024,
+                },currentelement[2],
+                currentelement[3],
+                currentelement[4],
+                currentelement[5],
+                currentelement[6],
+                currentelement[7]);
 
+        }
+    }
     socket.emit("reveive-joined",{
-        uid:receiverID
+        uid:receiverID,
     });
 });
 
@@ -63,11 +85,10 @@ document.querySelector("#drop_zone").addEventListener("change",function(e){
         let oldname=file.name;
         for (i=0; i<25; i++){
             filename+=oldname.charAt(i)
-
         }
         filename+="(...)."+((file.name.split('.')).pop());
     }
-
+    if (connected[0]===true){
     reader.onload = function(e){
         document.querySelector(".readyfor").classList.remove("active");
         document.querySelector(".fileok").classList.add("active");
@@ -98,6 +119,49 @@ document.querySelector("#drop_zone").addEventListener("change",function(e){
         el.querySelector('.keszpipa'),
         el.querySelector(".loader"),
         el.querySelector('.stopbutton'));
+    }
+    } else {
+        connected[1]=true;
+        connected[1]=true;
+        document.querySelector('.tick').setAttribute('style', "margin-top:5px")
+        document.querySelector('.waitingtext').setAttribute('style', "margin-top:15px")
+        reader.onload = function(e){
+            document.querySelector(".readyfor").classList.remove("active");
+            document.querySelector(".fileok").classList.add("active");
+            let buffer = new Uint8Array(reader.result);
+            let el =document.createElement("div");
+            el.classList.add("item");
+            el.innerHTML = `
+            <div class="filename">${filename}</div>
+            <div class="outstop">
+            <button class="stopbutton" onclick="megallit(this)" butid="${file.name}"> <img src="/docs/assets/stop.svg"  alt=""> </button>
+            <div class="loader"></div>  
+            <div class="progresscircle">
+            <div class="out-circle"><div class="in-circle"><span class="progress">0%</span></div></div>
+            </div>
+            <div class="keszpipa"> <img src="docs/assets/readytick.svg" alt="kesz"> </div>
+            
+            </div>
+        `;
+            document.querySelector(".fileok").appendChild(el);
+            let elementlist=[];
+            elementlist.push(el)
+            elementlist.push(file.name)
+            elementlist.push(buffer)
+            elementlist.push(el.querySelector(".progress"))
+            elementlist.push(el.querySelector('.in-circle'))
+            elementlist.push(el.querySelector('.keszpipa'))
+            elementlist.push(el.querySelector(".loader"))
+            elementlist.push(el.querySelector('.stopbutton'))
+            already.push(elementlist)
+
+            /*shareFile({},buffer,
+                el.querySelector(".progress"),
+                el.querySelector('.in-circle'),
+                el.querySelector('.keszpipa'),
+                el.querySelector(".loader"),
+                el.querySelector('.stopbutton'));*/
+        }
     }
     reader.readAsArrayBuffer(file);
 });
