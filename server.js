4c50731f2755eac65d1859448bf823db21615712
Balagaro <kiraly123balazs@gmail.com>
2024-03-27 09:53:36 +0100

    kukifix10

--- server.js
+++ server.js
@@ -464,12 +464,20 @@ io.on("connection", function(socket){
                         varrakas.push([results,curdb[1]])
                     });
                 } else{
+                    try{
                     //console.log(curid)
                     sql=`SELECT variations.type, tetelek.id, tetelek.megnev, variations.tetel_id, variations.value, variations.variation_id, tetelek.picture FROM variations,tetelek WHERE variations.tetel_id=tetelek.id and variations.variation_id=${con.escape(curdb[0])}`
                     con.query(sql, function (err, results, fields) {
                         if (err) throw err;
                         varrakas.push([results,curdb[1]])
-                    });}
+                    });}catch(e){
+                        sql=`SELECT variations.type, tetelek.id, tetelek.megnev, variations.tetel_id, variations.value, variations.variation_id, tetelek.picture FROM variations,tetelek WHERE variations.tetel_id=tetelek.id`
+                        con.query(sql, function (err, results, fields) {
+                            if (err) throw err;
+                            varrakas.push([results,curdb[1]])
+                        });
+                    }
+                }
             }
             setTimeout(function () {
                 socket.in('admin').emit('minem',[varrakas, titok])
